9cc74837e7de96a3f3f75f8763031005c58f196d
nmspaniol <nicolasmspaniol@gmail.com>
2026-01-30 21:57:52 -0300

    niri: updated script so that every empty workspace gets its name removed

--- .local/bin/unset_empty_ws_name.sh
+++ .local/bin/unset_empty_ws_name.sh
@@ -1,16 +1,5 @@
-prev_is_close=0
-
 niri msg --json event-stream | while read -r event; do
-    if jq -e 'select(.WindowClosed)' >/dev/null <<<"$event"; then
-        prev_is_close=1
-        continue
+    if jq -e 'select(.WindowFocusChanged and .WindowFocusChanged.id == null)' >/dev/null <<< "$event"; then
+        niri msg action unset-workspace-name
     fi
-
-    if jq -e 'select(.WindowFocusChanged and .WindowFocusChanged.id == null)' >/dev/null <<<"$event"; then
-        if [[ $prev_is_close -eq 1 ]]; then
-            niri msg action unset-workspace-name
-        fi
-    fi
-
-    prev_is_close=0
 done
