4012e27ad4d2dbc77f95ae78033f087a3d2a9686
nmspaniol <nicolasmspaniol@gmail.com>
2026-03-04 15:01:14 -0300

    niri: changed heuristic for clearing the workspace name in script

--- .local/bin/unset_empty_ws_name.sh
+++ .local/bin/unset_empty_ws_name.sh
@@ -1,5 +1,9 @@
+# TODO: verify if the workspace is really empty before unsetting instead of
+# doing the overview thing; taking a screenshot removes the name for now
 niri msg --json event-stream | while read -r event; do
-    if jq -e 'select(.WindowFocusChanged and .WindowFocusChanged.id == null)' >/dev/null <<< "$event"; then
-        niri msg action unset-workspace-name
+    if jq -e 'select(.WindowFocusChanged and .WindowFocusChanged.id == null)' >/dev/null <<<"$event"; then
+        if niri msg -j overview-state | jq -e '.is_open == false' >/dev/null; then
+            niri msg action unset-workspace-name
+        fi
     fi
 done
