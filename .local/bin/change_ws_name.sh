eb45788c14bf4df6dc4a392b45decd7541816043
nmspaniol <nicolasmspaniol@gmail.com>
2026-06-07 19:33:19 -0300

    niri: workspace renaming script now converts spaces to underlines

--- .local/bin/change_ws_name.sh
+++ .local/bin/change_ws_name.sh
@@ -1,7 +1,7 @@
 echo -ne "\n  Workspace name: "
 read NAME
 if [ -z "$NAME" ]; then
-  niri msg action unset-workspace-name;
+    niri msg action unset-workspace-name;
 else
-  niri msg action set-workspace-name $NAME
+    niri msg action set-workspace-name $(echo "$NAME" | tr " " _)
 fi
