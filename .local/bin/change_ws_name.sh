6cf961e90ab0a9d2b4f8b0820c56f14bb56a2d14
nmspaniol <nicolasmspaniol@gmail.com>
2026-07-15 19:30:58 -0300

    niri: minor script change from 'test' to bash's conditional syntax

--- .local/bin/change_ws_name.sh
+++ .local/bin/change_ws_name.sh
@@ -1,6 +1,6 @@
 echo -ne "\n  Workspace name: "
-read NAME
-if [ -z "$NAME" ]; then
+read -r NAME
+if [[ -z "$NAME" ]]; then
     niri msg action unset-workspace-name;
 else
     niri msg action set-workspace-name $(echo "$NAME" | tr " " _)
