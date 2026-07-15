d0435acc627cc22db016b1f3c37f1c3ab1707c93
nmspaniol <nicolasmspaniol@gmail.com>
2026-07-15 19:28:31 -0300

    niri: now unsetting workspace name only happens on window close, fixing commented issue

--- .local/bin/unset_empty_ws_name.sh
+++ .local/bin/unset_empty_ws_name.sh
@@ -1,9 +1,7 @@
-# TODO: verify if the workspace is really empty before unsetting instead of
-# doing the overview thing; taking a screenshot removes the name for now
 niri msg --json event-stream | while read -r event; do
-    if jq -e 'select(.WindowFocusChanged and .WindowFocusChanged.id == null)' >/dev/null <<<"$event"; then
-        if niri msg -j overview-state | jq -e '.is_open == false' >/dev/null; then
-            niri msg action unset-workspace-name
-        fi
+    if jq -e 'select(.WindowClosed)' >/dev/null <<<"$event"; then
+        if niri msg --json workspaces | jq -e '.[] | select(.is_active and (.active_window_id | not))'; then
+    		niri msg action unset-workspace-name
+    	fi
     fi
 done
